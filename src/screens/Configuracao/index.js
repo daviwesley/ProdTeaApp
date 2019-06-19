5aa347ae5dde9adfdabfc22506ff23fc58b3867a
Davi Wesley <davi.wesley@icloud.com>
2019-06-18 22:51:48 -0300

    remove pt-br character

--- src/screens/Configuracao/index.js
+++ src/screens/Configuracao/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Text, View } from "react-native";
 
-export default class Configuração extends Component {
+export default class Configuracao extends Component {
   render() {
     return (
       <View>
