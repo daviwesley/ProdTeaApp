9c4867c482c6fc6f6d4e4f410dc56559d3f83be9
daviwesley <davi.wesley@icloud.com>
2019-07-27 17:56:32 -0300

    configure action login and remove old reducer

--- src/redux/ducks/loginAction.js
+++ src/redux/ducks/loginAction.js
@@ -6,11 +6,12 @@ export const types = {
   LOGGED_IN: "auth/LOGGED",
   CREATE_USER_SUCCESS: "auth/CREATE",
   CREATE_USER_FAIL: "auth/USER_FAIL",
-  LOGGIN_USER_SUCCESS: "auth/USER_SUCCESS",
+  LOGIN_USER_SUCCESS: "auth/USER_SUCCESS",
   LOGIN_USER_FAIL: "auth/LOGIN_FAIL",
-  LOGIN_LOADING: "auth/LOGIN_LOADING"
+  LOGIN_LOADING: "auth/LOGIN_LOADING",
+  LOGGED_IN: "auth/LOGGED_IN"
 };
-const INITIAL = { loading: false, loggedIn: false };
+const INITIAL = { loading: false, loggedIn: false, error: '', user: null };
 //reducer
 const auth = (state = INITIAL, action) => {
   switch (action.type) {
@@ -25,12 +26,14 @@ const auth = (state = INITIAL, action) => {
     case types.LOGIN_LOADING:
       return {
         ...state,
-        loading: true
+        loading: action.status
       };
+    case types.LOGGED_IN:
+      return { ...state, loggedIn: action.status};
     case types.LOGIN_USER_SUCCESS:
-      return { ...state, loggedIn: true, loading: false };
+      return { ...state, loggedIn: true, error: '', user: action.user };
     case types.LOGIN_USER_FAIL:
-      return { ...state, error: action.error };
+      return { ...state, error: action.error, loggedIn: false };
     default:
       return state;
   }
@@ -49,14 +52,15 @@ export const createUserFail = error => {
     error
   };
 };
-export const loginLoading = () => {
+export const loginLoading = status => {
   return {
-    type: types.LOGIN_LOADING
+    type: types.LOGIN_LOADING,
+    status
   };
 };
 export const loginUserSuccess = resp => {
   return {
-    type: types.LOGGIN_USER_SUCCESS,
+    type: types.LOGIN_USER_SUCCESS,
     user: resp
   };
 };
@@ -68,8 +72,15 @@ export const loginUserFail = error => {
   };
 };
 
+export const loggedIn = status => {
+  return {
+    type: types.LOGGED_IN,
+    status
+  };
+};
+
+
 export const createUser = (email, pass) => dispatch => {
-  dispatch(loginLoading());
   firebase
     .auth()
     .createUserWithEmailAndPassword(email, pass)
@@ -80,19 +91,20 @@ export const createUser = (email, pass) => dispatch => {
 };
 
 export const loginUser = (email, pass) => dispatch => {
+  dispatch(loginLoading(true));
   firebase
     .auth()
     .signInWithEmailAndPassword(email, pass)
     .then(resp => {
+      dispatch(loggedIn(true));
       dispatch(loginUserSuccess(resp));
+      dispatch(loginLoading(false));
     })
     .catch(error => {
       dispatch(loginUserFail(error.code));
+      dispatch(loginLoading(false));
     });
 };
 
-export const dispatchLogin = () => {
-  return { type: types.LOGIN_USER_SUCCESS };
-};
 
 export default auth;
