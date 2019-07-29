2d1ad7a1c3869d9f12f9904169c19bdcaa97356c
daviwesley <davi.wesley@icloud.com>
2019-07-29 11:08:16 -0300

    remove unecessary types from old architeture

--- src/redux/ducks/types.js
+++
@@ -1,8 +0,0 @@
-export const START_LOAD = "START_LOAD";
-export const STOP_LOAD = "STOP_LOAD";
-export const LOGGED_IN = "LOGGED_IN";
-export const CREATE_USER_SUCCESS = "CREATE_USER_SUCCESS";
-export const CREATE_USER_FAIL = "CREATE_USER_FAIL";
-export const LOGIN_USER_SUCCESS = "LOGIN_USER_SUCCESS";
-export const LOGIN_USER_FAIL = "LOGIN_USER_FAIL";
-export const LOGIN_LOADING = "LOGIN_LOADING";
